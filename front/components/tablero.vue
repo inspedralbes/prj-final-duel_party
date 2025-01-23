c7473c5ebcd2315fde1c2c3874d0cf77af5a5b4a
diego <a23diemujper>
2025-01-23 09:05:49 +0100

    tablero

--- front/components/tablero.vue
+++ front/components/tablero.vue
@@ -2,9 +2,18 @@
     <main>
 
 <div id="tablero">
-<div class="ficha" :style="{ marginTop: movimiento.top + 'px',
+<div class="ficha1" :class="{animacion: animacion  }" :style="{ marginTop: movimiento.top + 'px',
       marginLeft: movimiento.izq + 'px',
       }"> </div>
+      <div class="ficha2" :class="{animacion: animacion  }" :style="{ marginTop: movimiento.top + 'px',
+      marginLeft: movimiento.izq + 'px',
+      }"> </div>
+      <div class="ficha3" :class="{animacion: animacion  }" :style="{ marginTop: movimiento.top + 'px',
+      marginLeft: movimiento.izq + 'px',
+      }"> </div>
+<div class="ficha4" :class="{animacion: animacion  }" :style="{ marginTop: movimiento.top + 'px',
+      marginLeft: movimiento.izq + 'px',
+     }"></div>
 <img src="/tablero.jpeg" srcset="">
  
 </div>
@@ -22,7 +31,7 @@
 
 import { reactive, ref } from 'vue';
 import { Coordenadas } from '../static/tablero';
-
+const animacion = ref(false);
 const posicionActual= ref(0);
 const movimiento=reactive({top:0,izq:0});
 pintarFicha(); 
@@ -63,17 +72,37 @@ function actualizarPosicion(num) {
 
 
 function pintarFicha(){
+    animacion.value = true;
     let posicion = getPosicion();
     movimiento.top = posicion.x;
     movimiento.izq = posicion.y;
-
+    setTimeout(() => {
+  animacion.value = false;
+}, 200);
+    
 }
 
 </script>
 <style scoped>
+
+@keyframes crecer {
+      0% {
+        transform: scale(1);  
+      }
+      100% {
+        transform: scale(1.5);  
+      }
+    }
+
+
+.animacion{
+    animation: crecer 0.1s infinite alternate;
+}
+
+
 #tablero {
     border: 1px solid black;
-  width: fit-content; /* El div se ajustará al contenido de la imagen */
+  width: fit-content;  
   margin: 20px auto; 
   text-align: center;
  
@@ -84,10 +113,40 @@ function pintarFicha(){
     height: auto;
      
 }
-.ficha{
+.ficha1{
     height: 20px;
     width: 20px;
     background-color: red;
+    border-radius: 50%;
+    position: absolute;
+    transition: all 0.2s ease-in;
+    border: 1px solid black; 
+}
+
+.ficha2{
+    height: 20px;
+    width: 20px;
+    background-color: blue;
+    border-radius: 50%;
+    position: absolute;
+    transition: all 0.2s ease-in;
+    border: 1px solid black; 
+}
+
+.ficha3{
+    height: 20px;
+    width: 20px;
+    background-color: yellow;
+    border-radius: 50%;
+    position: absolute;
+    transition: all 0.2s ease-in;
+    border: 1px solid black; 
+}
+
+.ficha4{
+    height: 20px;
+    width: 20px;
+    background-color: green;
     border-radius: 50%;
     position: absolute;
     transition: all 0.2s ease-in;
