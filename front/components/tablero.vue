4ecbfb7e780d56ab50b5f25720e98418f0f28681
diego <a23diemujper>
2025-01-23 13:24:51 +0100

    posiciones_EMPEZADO

--- front/components/tablero.vue
+++ front/components/tablero.vue
@@ -1,11 +1,22 @@
 <template>
     <main>
-{{ turno }}
+       
+        <div class="rojo"> Rojo {{ movimiento[0].posicion }} </div>
+         <div class="azul"> Azul {{ movimiento[1].posicion }} </div>
+         <div class="verde"> Verde {{ movimiento[2].posicion }} </div>
+         <div class="amarillo"> Amarillo {{ movimiento[3].posicion }} </div>
 
 <div id="tablero">
-   <div v-if="explosion" class="explosion"  :style="{ marginTop: movimiento[turno].top + 'px',
-      marginLeft: movimiento[turno].izq + 'px',
+    <div v-if="turno === 0">Rojo</div>
+        <div v-if="turno === 1">Azul</div>
+        <div v-if="turno === 3">Verde</div>
+        <div v-if="turno === 2">Amarillo</div>
+   <div v-if="explosion" class="explosion"  :style="{ marginTop: movimiento[turno-1<0?3 : turno-1].top + 'px',
+      marginLeft: movimiento[turno-1<0?3 : turno-1].izq + 'px',
       }"> <img  src="/explo.gif" alt="GIF"> </div>
+
+
+
 <div class="ficha1" :class="{animacion: movimiento[0].animacion  }" :style="{ marginTop: movimiento[0].top + 'px',
       marginLeft: movimiento[0].izq + 'px',
       }"> </div>
@@ -22,12 +33,12 @@
  
 </div>
 
-<button @click="actualizarPosicion(1)"> 1 </button>   
-<button @click="actualizarPosicion(2)"> 2 </button>   
-<button @click="actualizarPosicion(3)"> 3 </button>   
-<button @click="actualizarPosicion(4)"> 4 </button>   
-<button @click="actualizarPosicion(5)"> 5 </button>   
-<button @click="actualizarPosicion(6)"> 6 </button>   
+<button @click="actualizarPosicion(1)" :disabled="movimiento[turno].animacion" > 1 </button>   
+<button @click="actualizarPosicion(2)" :disabled="movimiento[turno].animacion" > 2 </button>   
+<button @click="actualizarPosicion(3)" :disabled="movimiento[turno].animacion" > 3 </button>   
+<button @click="actualizarPosicion(4)" :disabled="movimiento[turno].animacion" > 4 </button>   
+<button @click="actualizarPosicion(5)" :disabled="movimiento[turno].animacion" > 5 </button>   
+<button @click="actualizarPosicion(6)" :disabled="movimiento[turno].animacion" > 6 </button>   
 
 </main>
 </template>
@@ -40,8 +51,10 @@ const explosion = ref(false);
 const turno = ref(0);
 const nJugadores= ref(3);
 const posicionActual= reactive([-1,-1,-1,-1]); 
-const movimiento = reactive([{top:60,izq:90,animacion:false},{top:60,izq:60,animacion:false},{top:90,izq:90,animacion:false},{top:90,izq:60,animacion:false}]);
+const movimiento = reactive([{top:60,izq:90,animacion:false,posicion:1},{top:60,izq:60,animacion:false,posicion:2},{top:90,izq:90,animacion:false,posicion:3},{top:90,izq:60,animacion:false,posicion:4}]);
  
+
+
 
 function getPosicion(num){
     let x = Coordenadas[posicionActual[num]][0];
@@ -84,7 +97,7 @@ function actualizarPosicion(num) {
         }
     } 
     moverFicha();
-     
+   
 }
 
 function dosEnCasilla(num){
@@ -145,6 +158,32 @@ function pintarFicha(num){
         transform: scale(1.5);  
       }
     }
+
+.rojo{width: 200px; height: 100px; position: absolute;
+top: 0px;
+left: 0px;
+border: 1px solid black;
+}
+.azul{width: 200px; height: 100px;
+position: absolute;
+top: 0px;
+right: 0px;
+border: 1px solid black;
+}
+.verde{width: 200px; height: 100px;
+position: absolute;
+bottom: 0px;
+left: 0px;
+border: 1px solid black;
+
+}
+.amarillo{width: 200px; height: 100px;
+position: absolute;
+bottom: 0px;
+right: 0px;
+border: 1px solid black;
+}
+
 
 .explosion{
     width: auto;
